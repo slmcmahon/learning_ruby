10ae97351090f99a6e5b881bd37f09453f108439
Stephen L. McMahon <stephen@slmcmahon.com>
2015-01-18 06:46:07 -1000

    Moved data load logic into a class method in Point.

--- app.rb
+++ app.rb
@@ -10,21 +10,6 @@ require_relative 'models/Point'
 dbconfig = YAML::load(File.open('config/database.yaml'))
 ActiveRecord::Base.establish_connection(dbconfig['development'])
 
-def load_data
-  library_data = JSON.parse(open("http://data.hawaii.gov/api/views/jx86-2vch/rows.json?accessType=DOWNLOAD").read)
-  library_data["data"].each do |lib|
-    loc = lib[11]
-    addr = JSON.parse(loc[0])
-
-    p = Point.new
-    p.name = lib[8]
-    p.city = addr["city"]
-    p.latitude = loc[1]
-    p.longitude = loc[2]
-    p.save
-  end
-end
-
 def display_data(points)
   points.each do |p|
     puts "#{p.id}: #{p.name} in #{p.city} positioned at #{p.latitude}/#{p.longitude}"
@@ -33,7 +18,7 @@ end
 
 p = Point.all
 if (p.count == 0)
-  load_data
+  Point.load_from_source
   p = Point.all
   display_data(p)
 else
--- models/point.rb
+++ models/point.rb
@@ -1,2 +1,17 @@
 class Point < ActiveRecord::Base
+  DATA_URL = "http://data.hawaii.gov/api/views/jx86-2vch/rows.json?accessType=DOWNLOAD"
+  def self.load_from_source()
+    library_data = JSON.parse(open(DATA_URL).read)
+    library_data["data"].each do |lib|
+      loc = lib[11]
+      addr = JSON.parse(loc[0])
+
+      p = Point.new
+      p.name = lib[8]
+      p.city = addr["city"]
+      p.latitude = loc[1]
+      p.longitude = loc[2]
+      p.save
+    end
+  end
 end
